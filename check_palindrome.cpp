5ffc95c7ce76ca6570e1b41c14bd1741c6dda5d3
VARUN KAPASIA <96992611+KAPASIAVar@users.noreply.github.com>
2023-10-17 22:22:37 +0530

    Create check_palindrome.cpp
    Optimized solution of check palindrome in linked list with
    time comp-0(N)
    space comp-0(1).

---
+++ check_palindrome.cpp
@@ -0,0 +1,37 @@
+node * mid(node *head){
+    
+    node *slow=head;
+    node *fast=head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+node *rev(node* &head){
+    node *prev=NULL;
+    node *curr=head;
+    node *nxt=NULL;
+    while(curr!=NULL){
+        nxt=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nxt;
+    }
+    return prev;
+}
+bool check(node * &head){
+    if(head->next==NULL)return true;
+    node *middle=mid(head);
+    node *temp=middle->next;
+    
+    middle->next=rev(temp);
+    node *head1=head;
+    node *head2=middle->next;
+    while(head2!=NULL){
+        if(head2->data!=head1->data)return false;
+        head2=head2->next;
+        head1=head1->next;
+    }
+    return true;
+}
